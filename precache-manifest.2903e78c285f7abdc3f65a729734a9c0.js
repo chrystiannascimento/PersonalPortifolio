2e8299d49cd155bafb31ba54bb788971bbf552b5
Chrystian Gustavo <chrystiang20@gmail.com>
2020-05-21 12:13:23 -0300

    Updates

---
+++ precache-manifest.2903e78c285f7abdc3f65a729734a9c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a58205d920f77e66c290df44ef982bb",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "5ae19f159fecbafa9ce9",
+    "url": "/me/static/css/main.e8db8516.chunk.css"
+  },
+  {
+    "revision": "93063c666bf1e2f3fde7",
+    "url": "/me/static/js/2.75862615.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.75862615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae19f159fecbafa9ce9",
+    "url": "/me/static/js/main.f4565ccd.chunk.js"
+  },
+  {
+    "revision": "1ae5e8da1c0a54cc68aa",
+    "url": "/me/static/js/runtime-main.cc11463b.js"
+  }
+]);
